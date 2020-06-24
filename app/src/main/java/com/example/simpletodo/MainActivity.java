6d77e3553c470e1b500a01c5aaa90bb6a7d11519
Samuel Miserendino <samuelgm@fb.com>
2020-06-23 23:40:11 -0400

    Add toast, ability to add items

--- app/src/main/java/com/example/simpletodo/MainActivity.java
+++ app/src/main/java/com/example/simpletodo/MainActivity.java
@@ -5,10 +5,13 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
 import android.os.Bundle;
+import android.widget.Toast;
+
 import java.util.List;
 import java.util.ArrayList;
 
@@ -35,11 +38,29 @@ public class MainActivity extends AppCompatActivity {
         items.add("Have fun!");
 
 
-        ItemsAdapter itemsAdapter = new ItemsAdapter(items);
+        final ItemsAdapter itemsAdapter = new ItemsAdapter(items);
         rvItems.setAdapter(itemsAdapter);
 
-        // Puts things on the UI in a vertical way 
+        // Puts things on the UI in a vertical way
         rvItems.setLayoutManager((new LinearLayoutManager(this)));
+
+        btnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) { // Someone has tapped on our button
+
+                String todoItem = edItem.getText().toString();
+
+                // Add to the model
+                items.add(todoItem);
+
+                // Notify the adapter that we've inserted an item
+                itemsAdapter.notifyItemInserted(items.size() - 1 );
+                edItem.setText("");
+
+                // Toast to give user feedback that the item was added
+                Toast.makeText(getApplicationContext(), "Item was added", Toast.LENGTH_LONG).show();
+            }
+        });
 
     }
 }
