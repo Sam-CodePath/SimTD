f14926286e1c8a55ff2da1eafc3b979864769611
Sam <glorbajohanraraon@gmail.com>
2020-07-06 13:59:56 -0400

    add stretch goals -- you can click on an item to edit it

---
+++ app/src/main/java/com/example/simpletodo/EditActivity.java
@@ -0,0 +1,50 @@
+package com.example.simpletodo;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class EditActivity extends AppCompatActivity {
+
+    EditText etItem;
+    Button btnSave;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit);
+
+        etItem = findViewById(R.id.etItem);
+        btnSave = findViewById(R.id.btnSave);
+
+        getSupportActionBar().setTitle("Edit Item");
+
+
+        etItem.setText(getIntent().getStringExtra(MainActivity.KEY_ITEM_TEXT));
+
+        // When the user is done editing
+        btnSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Create an intent which will contain the results of whatever the user has modified
+                Intent intent = new Intent();
+
+                // Pass the results
+                intent.putExtra(MainActivity.KEY_ITEM_TEXT, etItem.getText().toString());
+                intent.putExtra(MainActivity.KEY_ITEM_POSITION, getIntent().getExtras().getInt(MainActivity.KEY_ITEM_POSITION));
+
+                // Set the result of the intent
+                setResult(RESULT_OK, intent);
+
+                // Finish the activity (close the current screen & go back)
+                finish();
+            }
+        });
+    }
+
+}
--- app/src/main/java/com/example/simpletodo/MainActivity.java
+++ app/src/main/java/com/example/simpletodo/MainActivity.java
@@ -1,9 +1,11 @@
 package com.example.simpletodo;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -22,6 +24,11 @@ import java.io.File;
 import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String KEY_ITEM_TEXT = "item_text";
+    public static final String KEY_ITEM_POSITION = "item_position";
+    public static final int EDIT_TEXT_CODE = 20;
+
 
     List<String> items;
 
@@ -58,7 +65,21 @@ public class MainActivity extends AppCompatActivity {
             }
         };
 
-        itemsAdapter = new ItemsAdapter(items, onLongClickListener);
+        ItemsAdapter.OnClickListener onClickListener = new ItemsAdapter.OnClickListener() {
+            @Override
+            public void onItemClicked(int position) {
+                Log.d("MainActivity", "Single click at position " + position);
+                // Create the new activity
+                Intent i = new Intent(MainActivity.this, EditActivity.class);
+                // Pass the data being edited
+                i.putExtra(KEY_ITEM_TEXT, items.get(position));
+                i.putExtra(KEY_ITEM_POSITION, position);
+                // Display the activity
+                startActivityForResult(i, EDIT_TEXT_CODE);
+            }
+        };
+
+        itemsAdapter = new ItemsAdapter(items, onLongClickListener, onClickListener);
 
         rvItems.setAdapter(itemsAdapter);
 
@@ -87,6 +108,30 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+
+    }
+
+    // Handle the result of the edit activity
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode == RESULT_OK && requestCode == EDIT_TEXT_CODE){
+            // Retrieve updated text value
+            String itemText = data.getStringExtra(KEY_ITEM_TEXT);
+            // Extract original position of edited item from the position key
+            int position = data.getExtras().getInt(KEY_ITEM_POSITION);
+
+            // update the model at the right position with the new item text
+            items.set(position, itemText);
+            // notify adapter that something has changed
+            itemsAdapter.notifyItemChanged(position);
+            // persist the changes
+            saveItems();
+            Toast.makeText(getApplicationContext(), "Item updated successfully!", Toast.LENGTH_LONG).show();
+        } else {
+            Log.w("MainActivity", "Unknown call to onAcativityResult");
+        }
 
     }
 
